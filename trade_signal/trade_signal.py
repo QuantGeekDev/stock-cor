86c7b5edd9067b9e1dc820396ff4cd04034fcf20
Alex Andru <alex007d@gmail.com>
2024-02-07 18:24:43 +0100

    Add handlers for order execution

--- trade_signal/trade_signal.py
+++ trade_signal/trade_signal.py
@@ -5,9 +5,21 @@ from dataclasses import dataclass
 @dataclass
 class TradeSignal:
     """ Class for holding information regarding the generated TradeSignal"""
-    def __init__(self, symbol, quantity, price):
-        self.symbol = symbol
+
+    ticker: str
+    quantity: float
+    ask_price: float
+    bid_price: float
+    volume_weighted_average_price: float
+    signal: str
+    timestamp: datetime
+
+    def __init__(self, ticker, quantity, ask_price, bid_price, volume_weighted_average_price):
+        self.ticker = ticker
         self.quantity = quantity
-        self.price = price
-        self.timestamp = datetime.now()
+        self.volume_weighted_average_price = volume_weighted_average_price
+        self.ask_price = ask_price
+        self.bid_price = bid_price
+        self.timestamp = datetime.datetime.now()
+
 
