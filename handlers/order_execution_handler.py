4f359f07ab5875156449090a69c252888b72270b
Alex Andru <alex007d@gmail.com>
2024-02-08 12:11:37 +0100

    Add RiskManagerHandler

--- handlers/order_execution_handler.py
+++ handlers/order_execution_handler.py
@@ -5,5 +5,5 @@ from trade_signal.trade_signal import TradeSignal
 class OrderExecutionHandler(AbstractHandler):
     def handle(self, trade_signal: TradeSignal):
         if trade_signal.signal:
-            print(f"Order Executed: {trade_signal.ticker}:{trade_signal.quantity} share at {trade_signal.ask_price} ")
+            print(f"Order Executed: {trade_signal.signal} {trade_signal.ticker}. {trade_signal.quantity} share at ${trade_signal.ask_price} ")
             super().handle(trade_signal)
